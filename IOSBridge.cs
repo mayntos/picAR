53cd6e7f147f4d73da83e344d7b3292fdd7ff54e
maynard santos <maynardcsantos@gmail.com>
2017-12-31 16:23:48 -0500

    Add files via upload

---
+++ IOSBridge.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine.SceneManagement;
+
+public class IOSBridge : MonoBehaviour
+{
+    //Pulls the plugin function from .mm file
+    [DllImport("__Internal")]
+    private static extern void OpenImagePicker(string game_object_name, string function_name); 
+
+    //helper function to allow ButtonDisplay.cs to communicate with this script.
+    public void OpenImagePickerHelper()
+    {
+        OpenImagePicker(gameObject.name, "ImagePicked");
+    }
+
+    //OpenImagePicker returns the NSURL location of the selected picture
+    //to this method. The static picPath variable stores that valuefor rendering.
+    void ImagePicked(string path)
+    {
+        PicScript.picPath = path;
+        SceneManager.LoadScene("ARTestScene");
+    }
+
+
+    //Loads the vuforia scene.
+    public void LoadCameraScene()
+    {
+        SceneManager.LoadScene("ARTestScene");
+    }
+}
---
+++ PicScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PicScript : MonoBehaviour {
+
+    public static string picPath;
+    private Renderer renderer;
+
+    private void Awake()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(AssignTexture(picPath));
+    }
+
+    private IEnumerator AssignTexture(string path)
+    {
+        using (WWW www = new WWW(path))
+        {
+            yield return www;                               //this line loads the texture into the www
+            renderer.material.mainTexture = www.texture;    //this assigns the texture.
+        }
+    }
+
+}
