c91dd9083a36d8084194631d82d844cdb82ae400
maynard santos <maynardcsantos@gmail.com>
2018-01-15 17:54:21 -0500

    updated PicScript to utilize scripts from exif lib

--- PicScript.cs
+++ PicScript.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PicScript : MonoBehaviour {
 
+
+
     public static string picPath;
     private Renderer renderer;
+    private Transform tref;
 
     private void Awake()
     {
         renderer = gameObject.GetComponent<Renderer>();
+        tref = gameObject.GetComponent<Transform>();
     }
 
     private void Start()
@@ -19,9 +23,28 @@ public class PicScript : MonoBehaviour {
 
     private IEnumerator AssignTexture(string path)
     {
-        using (WWW www = new WWW(path))
+        using (WWW www = new WWW(path))                     //WWW uses the IDisposable interface. "using" statement will Dispose of those object when the final curly brace is reached.
         {
             yield return www;                               //this line loads the texture into the www
+            ExifLib.JpegInfo jpi = ExifLib.ExifReader.ReadJpeg(www.bytes, "Foo");
+            switch (jpi.Orientation)
+            {
+                case ExifLib.ExifOrientation.TopRight:
+                    tref.localEulerAngles = new Vector3(90f, 90f, -90f);
+                    break;
+
+                case ExifLib.ExifOrientation.BottomLeft:
+                    tref.localEulerAngles = new Vector3(90f, 90f, 90f);
+                    break;
+
+                case ExifLib.ExifOrientation.BottomRight:
+                    tref.localEulerAngles = new Vector3(90f, 90f, 180f);
+                    break;
+                default:
+                    break;
+
+            }
+            
             renderer.material.mainTexture = www.texture;    //this assigns the texture.
         }
     }
